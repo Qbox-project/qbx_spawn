09a000d97e08cc30bd15ad9ac7949f8dd4cf466d
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-09-21 03:27:26 -0400

    refactor: add modules, ox_lib (#10)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,14 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-Spawn'
+description 'QBX-Spawn'
+repository 'https://github.com/Qbox-project/qbx-spawn'
 version '1.0.0'
 
 shared_scripts {
 	'config.lua',
 	'@ox_lib/init.lua',
+	'@qbx-core/import.lua',
 	'@qbx-apartments/config.lua'
 }
 
@@ -18,6 +20,11 @@ server_scripts {
 
 ui_page 'html/index.html'
 
+modules {
+    'qbx-core:core',
+    'qbx-core:playerdata'
+}
+
 files {
 	'html/index.html',
 	'html/style.css',
@@ -26,3 +33,5 @@ files {
 }
 
 lua54 'yes'
+
+use_experimental_fxv2_oal 'yes'
