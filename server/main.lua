37b95f92554b8e308955010f4403bd304ad735af
David Malchin <malchin459@gmail.com>
2024-09-26 16:16:19 +0300

    fix: load property spawns instead of house spawns (#22)

--- server/main.lua
+++ server/main.lua
@@ -3,16 +3,16 @@ lib.callback.register('qbx_spawn:server:getLastLocation', function(source)
     return json.decode(MySQL.single.await('SELECT position FROM players WHERE citizenid = ?', {player.PlayerData.citizenid}).position), player.PlayerData.metadata.currentPropertyId
 end)
 
-lib.callback.register('qbx_spawn:server:getHouses', function(source)
+lib.callback.register('qbx_spawn:server:getProperties', function(source)
     local player = exports.qbx_core:GetPlayer(source)
     local houseData = {}
-    local playerHouses = MySQL.query.await('SELECT house FROM player_houses WHERE citizenid = ?', {player.PlayerData.citizenid})
-    for i = 1, #playerHouses do
-        local name = playerHouses[i].house
-        local locationData = MySQL.single.await('SELECT `coords`, `label` FROM houselocations WHERE name = ?', {name})
+    local properties = MySQL.query.await('SELECT id, property_name, coords FROM properties WHERE owner = ?', {player.PlayerData.citizenid})
+    for i = 1, #properties do
+        local property = properties[i]
         houseData[#houseData + 1] = {
-            label = locationData.label,
-            coords = json.decode(locationData.coords).enter
+            label = property.property_name,
+            coords = json.decode(property.coords),
+            propertyId = property.id,
         }
     end
 
